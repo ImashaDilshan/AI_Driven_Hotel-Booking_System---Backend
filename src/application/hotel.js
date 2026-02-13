7a0f21c9d2a1a77762aca584056ad22e8f6fa957
Imasha_Dilshan <imashadilshan1080@gmail.com>
2026-02-14 00:13:51 +0530

    Hotel.js functions error handling

--- src/application/hotel.js
+++ src/application/hotel.js
@@ -2,104 +2,114 @@ import express from 'express';
 import Hotel from '../infrastucture/entities/Hotel.js';
 import e from 'express';
 import { parse } from 'dotenv';
+import NotFoundError from '../domain/errors/not_found_error.js';
+import ValidationError from '../domain/errors/validation_error.js';
+import UnauthorizeError from '../domain/errors/unauthorize_error.js';
 
 
 // In asynchronous operations, we interact with the database using Mongoose. no need to use the in-memory array for these operations.
 
 
 // Data Retrieval async operations
-export const getAllHotels = async (req,res) => {
+export const getAllHotels = async (req,res,next) => {
     try{
         const hotels = await Hotel.find();
         res.status(200).json(hotels);
     }catch(error){
-        res.status(500).send("Server Error !");
+        next(error); // Pass the error to the global error handler
     }
 }
 
-export const getHotelById = async (req , res) => {
+export const getHotelById = async (req , res, next) => {
     try{
-    const ID = parseInt(req.params._id);
+    const ID = req.params._id;
     const hotel = await Hotel.findById(ID);
     if(!hotel){
-        res.status(404).send("Hotel not found !");
+        // res.status(404).send("Hotel not found !");
+        throw new NotFoundError("Hotel not found !");
         return;
     }
     res.status(200).json(hotel);
     }catch(error){
-        res.status(500).send(error.message);
+        next(error); 
     }
 }
 
 //Data Creation async operation
-export const createHotel = async (req,res) => {
+export const createHotel = async (req,res,next) => {
     try{
     const newhotel = req.body;
     if(!newhotel.name || !newhotel.image || !newhotel.location || !newhotel.rate || !newhotel.reviews || !newhotel.price){
-        res.status(400).send("All fields are required !");
+       // res.status(400).send("All fields are required !");
+        throw new ValidationError("All fields are required !");
         return;
     }
     await Hotel.create(newhotel);
     res.status(201).send("New hotel added successfully !");
     }catch(error){
-        res.status(500).send(error.message);
+        next(error);
     }   
 }
 
 // Data Update operations
-export const updateHotel =  async (req,res) => {
+export const updateHotel =  async (req,res,next) => {
     try{
         const _id = parseInt(req.params._id);
         const updatedHotel = req.body;
         if(!updatedHotel.name || !updatedHotel.image || !updatedHotel.location || !updatedHotel.rate || !updatedHotel.reviews || !updatedHotel.price){
-            res.status(400).send("All fields are required !");
+            //res.status(400).send("All fields are required !");
+            throw new ValidationError("All fields are required !");
             return;
         }
         const hotel = await Hotel.findById(_id);
         if(!hotel){
-            res.status(404).send("Hotel not found !");
+           // res.status(404).send("Hotel not found !");
+            throw new NotFoundError("Hotel not found !");
             return;
         }
         await Hotel.findByIdAndUpdate(_id, updatedHotel);
         res.status(200).send("Hotel updated successfully !");
     }catch(error){
-        res.status(500).send(error.message);
+        next(error);
     }
 }
 
-export const patchHotel = async (req,res) => {
+export const patchHotel = async (req,res,next) => {
     try{
         const _id = parseInt(req.params._id);
         const hotelUpdates = req.body;
         if(!hotelUpdates.price){
-            res.status(400).send("Price field is required for patching !");
+            // res.status(400).send("Price field is required for patching !");
+            throw new ValidationError("Price field is required for patching !");
             return;
         }
         const hotel = await Hotel.findById(_id);
         if(!hotel){
-            res.status(404).send("Hotel not found !");
+            // res.status(404).send("Hotel not found !");
+            throw new NotFoundError("Hotel not found !");
             return;
         }
         await Hotel.findByIdAndUpdate(_id, { price: hotelUpdates.price });
         res.status(200).send("Hotel price updated successfully !");
 
     }catch(error){
-        res.status(500).send(error.message);
+        next(error);
     }
 }  
 
 // Data Deletion operation
-export const deleteHotel = async (req,res) => {
+export const deleteHotel = async (req,res,next) => {
    try{
     const _id = parseInt(req.params._id);
     const hotel = await Hotel.findById(_id);
     if(!hotel){
-        res.status(404).send("Hotel not found !");
+        // res.status(404).send("Hotel not found !");
+        throw new NotFoundError("Hotel not found !");
         return;
     }
     await Hotel.findByIdAndDelete(_id);
     res.status(200).send("Hotel deleted successfully !");
    }catch(error){
-    res.status(500).send(error.message);
+    next(error);
    }
 }
