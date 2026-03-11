3a8b62286dbda59245a31b526a562d5089a89582
Imasha_Dilshan <imashadilshan1080@gmail.com>
2026-03-11 11:36:15 +0530

    Authenticated Locations API's

--- src/api/location.js
+++ src/api/location.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import { getAllLocations, getLocationById, createLocation, updateLocation, patchLocation, deleteLocation } from '../application/location.js';
+import isAuthenticated from './middlewar/authentication_middleware.js';
+
 
 const locationRouter = express.Router();
 
 locationRouter.route('/')
-    .get(getAllLocations)
-    .post(createLocation);
+    .get( getAllLocations)
+    .post( createLocation);
 
 locationRouter.route('/:_id')
-    .get(getLocationById)
-    .put(updateLocation)
-    .patch(patchLocation)
-    .delete(deleteLocation);
+    .get(isAuthenticated, getLocationById)
+    .put(isAuthenticated, updateLocation)
+    .patch(isAuthenticated, patchLocation)
+    .delete(isAuthenticated, deleteLocation);
 
 export default locationRouter;
