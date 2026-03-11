73245bdac7ab5693177e338e4b4c63709a2e915d
Imasha_Dilshan <imashadilshan1080@gmail.com>
2026-03-11 11:06:46 +0530

    Hotels API Authenticated

--- src/api/hotel.js
+++ src/api/hotel.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import { getAllHotels, getHotelById, createHotel, updateHotel, patchHotel, deleteHotel  }  from '../application/hotel.js';
+import isAuthenticated from './middlewar/authentication_middleware.js';
 
 const hotelRouter = express.Router();
 
 
 hotelRouter.route('/')
-    .get(getAllHotels)
-    .post(createHotel);
+    .get( getAllHotels)
+    .post( createHotel);
 
 hotelRouter.route('/:_id')
-    .get(getHotelById)
-    .put(updateHotel)
-    .patch(patchHotel)
-    .delete(deleteHotel);
+    .get(isAuthenticated, getHotelById)
+    .put(isAuthenticated, updateHotel)
+    .patch(isAuthenticated, patchHotel)
+    .delete(isAuthenticated, deleteHotel);
 
 export default hotelRouter;
